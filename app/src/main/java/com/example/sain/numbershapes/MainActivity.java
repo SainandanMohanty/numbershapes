c5c9aa962574fc11134fd2b19c4de4a054bad719
Sainandan Mohanty <sainandan.mohanty.sm@gmail.com>
2018-05-30 01:27:56 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/sain/numbershapes/MainActivity.java
@@ -0,0 +1,88 @@
+package com.example.sain.numbershapes;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onClick(View view) {
+        EditText editText = findViewById(R.id.editText);
+        int number = Integer.parseInt(editText.getText().toString());
+
+        if (number > 0) {
+            Number newNumber = new Number();
+            newNumber.value = number;
+
+            output(newNumber);
+        } else {
+            Toast.makeText(this, "Enter an integer greater than zero", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void output(Number newNumber) {
+        boolean isTriangular = newNumber.isTriangular();
+        boolean isSquare = newNumber.isSquare();
+        String message;
+
+        if (isTriangular && !isSquare) {
+            message = "The number is triangular";
+        } else if (!isTriangular && isSquare) {
+            message = "The number is square";
+        } else if (isTriangular && isSquare) {
+            message = "The number is both triangular and square";
+        } else {
+            message = "The number is neither triangular nor square";
+        }
+
+        Toast.makeText(this, message, Toast.LENGTH_LONG).show();
+    }
+
+    class Number {
+        int value;
+
+        private boolean isTriangular() {
+
+            int x = 1;
+            int sum = 0;
+
+            while (sum < value) {
+                sum += x;
+
+                if (sum == value) {
+                    return true;
+                }
+
+                x++;
+            }
+
+            return false;
+        }
+
+        private boolean isSquare() {
+
+            int x = 1;
+            int square = 0;
+
+            while (square < value) {
+                square = x * x;
+
+                if (square == value) {
+                    return true;
+                }
+
+                x++;
+            }
+
+            return false;
+        }
+    }
+}
